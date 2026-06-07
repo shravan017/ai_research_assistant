a55d5604bcd4a70ce7e6f7c94b14c5998b3234a9
shravan017 <shravannaik027@gmail.com>
2026-06-07 20:55:41 +0530

    Setting up the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-black text-white flex items-center justify-center">
+        <h1 className="text 4xl font-bold">Research Assistant</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
